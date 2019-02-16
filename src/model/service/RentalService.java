22cd0b9c3828394675e5b1e29a7b5845059fa14e
DavidRicardoJ <david_ricardo@live.com>
2019-02-16 19:21:46 -0200

    Solution with interfaces.

--- src/model/service/RentalService.java
+++ src/model/service/RentalService.java
@@ -1,5 +1,6 @@
 package model.service;
 
+import model.TaxService;
 import model.entities.CarRental;
 import model.entities.Invoice;
 
@@ -7,9 +8,9 @@ public class RentalService {
     private Double pricePerDay;
     private Double pricePerHour;
 
-    private BrazilTaxService taxService;
+    private TaxService taxService;
 
-    public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+    public RentalService(Double pricePerDay, Double pricePerHour, TaxService taxService) {
         this.pricePerDay = pricePerDay;
         this.pricePerHour = pricePerHour;
         this.taxService = taxService;
